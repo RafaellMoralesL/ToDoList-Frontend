4b83a87279bcefd28cccb5ae457f9bf4e795a6c1
RafaellMoralesL <25001309@galileo.edu>
2026-04-23 20:44:58 -0600

    semana2

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,10 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Form.scss';
+import { useTaskStore } from '../../store/taskStore';
+import { useGoalStore } from '../../store/goalStore';
+import { useMenuStore } from '../../store/menuStore';
+import { useRef } from 'react';
 
 type FormTaskAndGoalProps = {
     onAdd?: () => void;
@@ -8,11 +12,30 @@ type FormTaskAndGoalProps = {
 
   function FormTaskAndGoal( { onAdd }: FormTaskAndGoalProps) {
 
+    const inputRefName = useRef<HTMLInputElement>(null);
+    const inputRefDescription = useRef<HTMLTextAreaElement>(null);
+    const inputRefDueDate = useRef<HTMLInputElement>(null);
+    const isActiveMenu = useMenuStore((state) => state.menu.active);
+    const addTask = useTaskStore((state) => state.addTask);
+    const addGoal = useGoalStore((state) => state.addGoal);
+
     const handleSubmit = (e: React.SubmitEvent) => {
       e.preventDefault();
-      if (onAdd) {
-        onAdd();
-      }
+      const name = inputRefName.current?.value;
+      const description = inputRefDescription.current?.value;
+      const dueDate = inputRefDueDate.current?.value;
+            if (name && description && dueDate) {
+              if (isActiveMenu === 'tasks') {
+                addTask({ id: Date.now(), name, description, dueDate });
+              } else {
+                addGoal({ id: Date.now(), name, description, dueDate });
+              }
+
+              if (onAdd) {
+                  onAdd();
+          }
+          }
+
     }
 
   return (
@@ -21,17 +44,17 @@ type FormTaskAndGoalProps = {
 
         <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control type="text" ref={inputRefName} />
         </Form.Group>
 
         <Form.Group className="mb-3">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={3} ref={inputRefDescription} />
         </Form.Group>
 
         <Form.Group className="mb-3">
             <Form.Label>Due Date</Form.Label>  
-            <Form.Control type="date" />
+            <Form.Control type="date" ref={inputRefDueDate} />
         </Form.Group>
 
         <Button type="submit" variant="info">
